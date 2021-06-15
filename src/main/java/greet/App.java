e6b86f9b5c6014db66e8c5a1a0becf7a61ba6020
Ammaar <fakirammaar8@gmail.com>
2021-06-15 09:54:37 +0200

    added handlebars file

--- src/main/java/greet/App.java
+++ src/main/java/greet/App.java
@@ -14,20 +14,28 @@ public class App {
 
     public static void main(String[] args) {
         staticFiles.location("/public");
-        get("/greet", (req, res) -> {
-
-                String user = req.queryParams("username");
-        Greet greet = new Greet();
-        return greet.greetUser(user);}
-        );
-        get("/greet/:username", (req, res) -> {
-                    String user = req.queryParams("username");
-                    Greet greet = new Greet();
-                    return greet.greetUser(user);
-                }
+        get("/hello", (req, res) -> {
+            return "";
+        });
 
 
-        );
+        post("/hello", (req, res) -> {
+            return "";
+        });
+//        get("/greet", (req, res) -> {
+//
+//                String user = req.queryParams("username");
+//        Greet greet = new Greet();
+//        return greet.greetUser(user);}
+//        );
+//        get("/greet/:username", (req, res) -> {
+//                    String user = req.queryParams("username");
+//                    Greet greet = new Greet();
+//                    return greet.greetUser(user);
+//                }
+//
+
+//        );
 
 //        get("/home", (req, res) -> {
 //            Map<String, Object> model = new HashMap<>();
